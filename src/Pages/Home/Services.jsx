414bbf27daae81c449cb1af8b170ab0201a9673a
MdAbdullahIbnNoor <ab.89noor@gmail.com>
2023-11-02 20:35:24 +0600

    JWT token done

--- src/Pages/Home/Services.jsx
+++ src/Pages/Home/Services.jsx
@@ -18,7 +18,7 @@ const Services = () => {
     }, []);
 
 
-    console.log(services);
+    // console.log(services);
     return (
         <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
             <div className="text-center mb-10">
